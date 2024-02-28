d2a1928691413d9dc9b68210c9ef5f2335818023
Nirjhor <tanveer.ahmed@technonext.com>
2024-02-28 18:09:58 +0600

    Add project files.

---
+++ ExpenseTrackerApi/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using ExpenseTrackerApi.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ExpenseTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("Admins")]
+        [Authorize]
+        public string AdminsEndPoint()
+        {
+           // var currentUser = GetCurrentUser();
+           // return Ok($"HI {currentUser.UserName},you are an{currentUser.Role}");
+           return "401";
+        }
+
+        [HttpGet("Public")]
+       public IActionResult Public()
+        {
+            return Ok("Hi ");
+        }
+      /*  private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    UserName = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+
+            }
+            else return null;
+        }
+      */
+    }
+}
---
+++ ExpenseTrackerApi/Model/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpenseTrackerApi.Model
+{
+    public class UserLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
