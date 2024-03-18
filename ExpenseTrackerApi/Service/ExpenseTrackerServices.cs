861c7c763911a12480ab966d1bdef63bf250a215
Nirjhor <tanveer.ahmed@technonext.com>
2024-03-18 12:44:38 +0600

    log edited

--- ExpenseTrackerApi/Service/ExpenseTrackerServices.cs
+++ ExpenseTrackerApi/Service/ExpenseTrackerServices.cs
@@ -31,18 +31,23 @@ namespace ExpenseTrackerApi.Service.Implementations
             {
                 Log log = new Log
                 {
-                    LogInformation = "User Added Spending",
+                    LogInformation = "Expenditure exceeds deposit amount",
                     TimeStamp = DateTime.Now
                 };
                 var logMsg = await _logService.CreateLog(log);
 
                 msg.Message = "Expenditure exceeds deposit amount";
                 msg.StatusCode = 200;
-                // return logMsg;
             }
             else
             {
-                msg.Message = "Sucessfully Added";
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully Added Spending",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
+                msg.Message = "Sucessfully Spending";
             }
             return msg;
 
@@ -60,10 +65,22 @@ namespace ExpenseTrackerApi.Service.Implementations
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == null)
             {
+                Log log = new Log
+                {
+                    LogInformation = "Faild to get percentage",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Faild to get percentage";
             }
             else
             {
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully get percentage",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Sucessfully get percentage";
             }
             return (res, msg);
@@ -73,7 +90,6 @@ namespace ExpenseTrackerApi.Service.Implementations
         public async Task<long> GetTotalSum(int Userid, DateTime fromdate, DateTime toDate)
         {
             var res = await _repository.GetTotalSum(Userid, fromdate, toDate);
-            // var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             return res;
 
         }
@@ -84,31 +100,55 @@ namespace ExpenseTrackerApi.Service.Implementations
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == null || res.Count == 0)
             {
+                Log log = new Log
+                {
+                    LogInformation = "Failed to get data by user",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Failed to get data";
             }
             else
             {
+                Log log = new Log
+                {
+                    LogInformation = "Successfully got the data",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Successfully got the data";
             }
             return (msg, res);
         }
 
-        // public Task<long> Deposit(Deposit deposit)
+
 
 
 
         public async Task<MessageHelperModel> Deposit(Deposit deposit)
         {
-            // var res = await _UnitOfWorkRepository.Todo.CreateTaskAsync(CreateTaskModel);
             var res = await _repository.Deposit(deposit);
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == 0)
             {
-                msg.Message = "Faild To Create";
+                Log log = new Log
+                {
+                    LogInformation = "Failed to Deposit",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
+                msg.Message = "Failed to Deposit";
+               
             }
             else
             {
-                msg.Message = "Sucessfully Added";
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully Deposited",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
+                msg.Message = "Sucessfully Deposited";
             }
             return msg;
 
@@ -119,10 +159,23 @@ namespace ExpenseTrackerApi.Service.Implementations
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == 0)
             {
+                Log log = new Log
+                {
+                    LogInformation = "User Update data",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Failed To Update";
             }
             else
             {
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully Updated",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
+       
                 msg.Message = "Sucessfully Updated";
             }
             return msg;
@@ -135,26 +188,51 @@ namespace ExpenseTrackerApi.Service.Implementations
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == 0)
             {
+                Log log = new Log
+                {
+                    LogInformation = "Failed To Delete Row",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Failed To Delete Row";
             }
             else
             {
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully Deleted",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Sucessfully Deleted";
             }
             return msg;
 
         }
-        //Task<MessageHelperModel> GetSum(int UserId, string Category);
+
         public async Task<(MessageHelperModel, long)> GetSum(int UserId, string Category, DateTime fromDate, DateTime toDate)
         {
             var res = await _repository.GetSum(UserId, Category, fromDate, toDate);
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == 0)
             {
+                Log log = new Log
+                {
+                    LogInformation = "Failed To Retrive Information",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Failed To Retrive Information";
             }
             else
             {
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully Retrived",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
+
                 msg.Message = "Sucessfully Retrived";
             }
             return (msg, res);
@@ -169,10 +247,22 @@ namespace ExpenseTrackerApi.Service.Implementations
             var msg = new MessageHelperModel { StatusCode = 200, Message = "" };
             if (res == null)
             {
+                Log log = new Log
+                {
+                    LogInformation = "Failed To Delete Row",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Failed To Delete Row";
             }
             else
             {
+                Log log = new Log
+                {
+                    LogInformation = "Sucessfully Deleted",
+                    TimeStamp = DateTime.Now
+                };
+                var logMsg = await _logService.CreateLog(log);
                 msg.Message = "Sucessfully Deleted";
             }
             return res;
